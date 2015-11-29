85f6efe7aea135e1569d07956f7ff4053140b5ec
sscomw <sscomw@gmail.com>
2015-11-29 10:34:21 +0900

    shutter cam name

--- shutter.sh
+++ shutter.sh
@@ -2,4 +2,4 @@
 
 DATE="%Y%m%d%H%M00"
 
-fswebcam -S 34 -d /dev/video0 -r 800x600 --line-colour '#FF000000' --banner-colour 'FF000000' --jpeg 95 --title "PlantX-CAMxx@Yasainoie" --timestamp "%Y/%m/%d-%H:%M" /image/CAMxx-$DATE.jpg;
+fswebcam -S 34 -d /dev/video0 -r 800x600 --line-colour '#FF000000' --banner-colour 'FF000000' --jpeg 95 --title "CAMxx@place" --timestamp "%Y/%m/%d-%H:%M" /image/CAMxx-$DATE.jpg;
